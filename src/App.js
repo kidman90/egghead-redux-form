d9699227573ff1439ec519ad7648e93f502bb3fb
kidman90 <kacprzak90@gmail.com>
2019-02-07 09:52:14 +0100

    05 Set an Initial Value of a Form Field using redux-form

--- src/App.js
+++ src/App.js
@@ -6,8 +6,15 @@ class RegisterFormContainer extends Component {
     window.alert(JSON.stringify(values, null, 4));
   };
 
+  getInitialValues() {
+    return {
+      preference: 'spaces',
+      newsletter: true
+    };
+  }
+
   render() {
-    return <RegisterForm onSubmit={this.submit} />;
+    return <RegisterForm onSubmit={this.submit} initialValues={this.getInitialValues()} />;
   }
 }
 
