888ea8b9d3a65c4e05e1e7cb66c4124e5a491e95
kidman90 <kacprzak90@gmail.com>
2019-02-07 11:40:40 +0100

    08 Add Synchronous Validation to a redux-form using field-level Validation

--- src/validation/index.js
+++ src/validation/index.js
@@ -1,21 +1,12 @@
-export const validate = values => {
-  const errors = {};
+export const required = value =>
+  value ? undefined : 'Value is required';
 
-  if (!values.firstname) {
-    errors.firstname = 'First name is required';
-  }
+export const minLength = value =>
+  value.length < 4
+    ? 'Value must be at least 4 characters'
+    : undefined;
 
-  if (!values.surname) {
-    errors.surname = 'Surname name is required';
-  }
-
-  if (!values.username) {
-    errors.username = 'Username name is required';
-  } else if (values.username.length < 4) {
-    errors.username = 'Username must be at least 4 characters long';
-  } else if (values.username > 10) {
-    errors.username = 'Username is too long'
-  }
-
-  return errors;
-};
+export const maxLength = value =>
+  value.length > 10
+    ? 'Value is too long'
+    : undefined;
